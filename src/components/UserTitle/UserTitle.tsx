b8919ea514943a9a8a09357f6e754422640d9939
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2023-01-06 09:41:36 +0100

    add link to account

--- src/components/UserTitle/UserTitle.tsx
+++ src/components/UserTitle/UserTitle.tsx
@@ -12,11 +12,18 @@ const localDate = new Intl.DateTimeFormat("en-GB", {
 
 export const UserTitle = ({ created, login, name }: UserTitleProps) => {
   const joinedDate = localDate.format(new Date(created));
+  const gitHibUrlFirstPart = "https://github.com/";
 
   return (
     <div className={styles.userTitle}>
       <h2>{name}</h2>
-      <h3>{login}</h3>
+      <a
+        href={`${gitHibUrlFirstPart}${login}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h3>{login}</h3>
+      </a>
       <span>{joinedDate}</span>
     </div>
   );
